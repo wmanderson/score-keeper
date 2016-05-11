f4d9ddec94e8ed9ac0168eb7bfc060bd6b88f074
wmanderson <will@everlane.com>
2016-05-10 19:30:28 -0700

    Made awesome

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var n = 0;
 
-
+// My code is ultimate. Never forget
 
 
   $("#score").html(n + " Points");
@@ -13,17 +13,18 @@ $(document).ready(function() {
 
   $(document).on("click", "#decrease-5", function() {
       if (n >= 5) {
-      $("#score").html((n -= 5) +" Points");}
+      $("#score").html((n -= 5) +" Points");
+    }
 });
 
 
-
   $(document).on("click", "#submit-custom-score", function() {
-
       var entry = parseInt($("#custom-score").val());
       n = entry;
 
+      if (n >= 5) {
       parseInt($("#score").html(entry + " Points"));
+    }
   });
 
 
